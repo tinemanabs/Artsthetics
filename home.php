6dec78c9d97f60bd30d6f6a5689dd17b26ed348d
Christine Manabat <201811234@fit.edu.ph>
2021-04-12 00:02:51 +0800

    UserHome & Home Navbar done

---
+++ home.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    require_once './web/header.php';
+    ?>
+
+    <link rel="stylesheet" href="css/home.css?v=8">
+    <title>Home</title>
+</head>
+
+<body>
+    <?php
+    require_once './web/navbar_user.php';
+    ?>
+
+    <div class="gallery">
+        <div class="gallery-item">
+            <img src="img/gallery/image.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image.png">
+        </div>
+        
+        <div class="gallery-item">
+            <img src="img/gallery/image2.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image4.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image5.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image6.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image7.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image8.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image9.jpg">
+        </div>
+        
+        <div class="gallery-item">
+            <img src="img/gallery/image10.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image11.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image12.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image13.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image14.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image15.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image16.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image17.jpg">
+        </div>
+
+        <div class="gallery-item">
+            <img src="img/gallery/image18.jpg">
+        </div>
+    </div>
+
+
+</body>
+
+</html>
